c25babe281531780e7371f75c4b706eb3cae8db2
thogaki <thomasgabrielk@gmail.com>
2026-07-21 22:46:28 +0200

    Delete

--- assets/config.js
+++
@@ -1,7 +0,0 @@
-window.MT_CONFIG = Object.freeze({
-  supabaseUrl: "https://gqsiiftihedavogukazb.supabase.co",
-  supabasePublishableKey: "sb_publishable_c8zftVGghEkaavPOM3VISA_s4SXmoyo",
-  adminEmail: "thomasgabrielk@gmail.com",
-  environment: "development",
-  version: "2.1.1-dev.3"
-});
